86edabd93104540dd457b5dae143680241dbc373
Haggars <31191395+Haggars@users.noreply.github.com>
2020-07-07 15:31:37 +0100

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Enter Steam directory (surround with /):"
+echo "Enter path to Steam directory (surround with /):"
 read steamlib
 echo "Enter wine user (default for steam is 'steamuser':)"
 read user
@@ -8,7 +8,7 @@ export USER=$user
 echo Exported USER as $user
 echo Enter path to executable:
 read Exec
-echo Enter Steam game ID:
+echo Enter Steam game ID (10090 for World at War):
 read gameid
 echo Running executable:
 WINEPREFIX="$steamlib/Steam/steamapps/compatdata/$gameid/pfx/" wine "$Exec"
