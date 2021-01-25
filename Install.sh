88ad724f37810463b55e278771d1f72f883603cc
Haggars <31191395+Haggars@users.noreply.github.com>
2021-01-25 10:39:28 +0000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
-echo "Enter path to Steam directory (e.g. /run/media/User/Drive1):"
-read steamlib
-echo "Enter wine user (default for steam is 'steamuser':)"
-read user
-export USER=$user
-echo Exported USER as $user
-echo Enter path to executable:
-read Exec
-echo Enter Steam game ID (10090 for World at War):
-read gameid
-echo Running executable:
-WINEPREFIX="$steamlib/Steam/steamapps/compatdata/$gameid/pfx/" wine "$Exec"
+steamdir=$(zenity --file-selection --title "Select Steam Parent Folder" --directory)
+
+echo "$steamdir"
+
+wineuser=$(zenity --entry --text "Enter Wine User (default is steamuser)" --title "Wine User" --entry-text "")
+
+echo "$wineuser"
+
+exec=$(zenity --file-selection --file-filter=*.exe --title "Select Executable")
+
+echo "$exec"
+
+
+gameid=$(zenity --entry --text "Enter Steam game ID (10090 for World at War)" --title "Game ID" --entry-text "")
+
+echo "$gameid"
+
+
+
+
+
+(
+echo "# Running Installer."
+WINEPREFIX="$steamdir/Steam/steamapps/compatdata/$gameid/pfx" wine "$exec"
+
+echo "# All Done." ; sleep 2
+echo "100"
+) |
+zenity --progress \
+  --title="Installing" \
+  --text="Running Installer." \
+  --auto-close \
+  --pulsate
+
+(( $? != 0 )) && zenity --error --text="Error in zenity command."
